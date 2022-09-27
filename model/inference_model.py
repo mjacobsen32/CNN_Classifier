7d798fb899792d14c4fbfdc7ef8d8dcf9be421b0
Matthew Jacobsen <matthewjacobsen1999@gmail.com>
2022-09-27 09:12:50 -0700

    model classes

---
+++ model/inference_model.py
@@ -0,0 +1,5 @@
+
+
+class Inference_Model:
+    def __init__(self, path_to_model):
+        
---
+++ model/new_model.py
@@ -0,0 +1,32 @@
+import torch
+import torchvision
+
+
+class Model:
+    def __init__(self):
+
+    def save_model(self,path_to_save):
+        
+    def new_model(self,batch_size,optimization,loss,
+    learning_rate,scheduler,step_size,gamma,epochs,model):
+
+    def epoch(self):
+
+    def train(self):
+
+    def validation(self):
+
+    def test(self):
+
+    def get_weights(self):
+
+    def get_set_lengths(self):
+
+    def get_loss_func(self):
+
+    def get_optimization_func(self):
+
+    def get_lr_sched(self):
+
+        
+    
